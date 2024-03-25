762cc093464104e66ba5a7ffb4435418dfea1e04
sopiko abulidze <abulidze2019@gmail.com>
2024-03-25 14:07:44 +0000

    finish with product sorting

--- products/views.py
+++ products/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 # Create your views here.
 
 
+
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
@@ -22,7 +23,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -54,27 +56,7 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
-    """ A view to show all products, including sorting and search queries """
 
-    products = Product.objects.all()
-    query = None
-
-    if request.GET:
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
-
-    context = {
-        'products': products,
-        'search_term': query,
-    }
-
-    return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
