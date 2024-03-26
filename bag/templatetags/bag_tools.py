781c7a34386cf623dff02611905735e6ebc25d12
sopiko abulidze <abulidze2019@gmail.com>
2024-03-26 13:42:30 +0000

    finish with toasts

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -1,8 +1,14 @@
 from django import template
 
-
 register = template.Library()
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
-    return price * quantity
+    # Check if either price or quantity is a dictionary
+    if isinstance(price, dict) or isinstance(quantity, dict):
+        return 0  # Or any default value you prefer if one of them is a dictionary
+    else:
+        try:
+            return price * quantity
+        except TypeError:
+            return 0
--- products/views.py
+++ products/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.db.models import Q
-from .models import Product,Category
 from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .models import Product, Category
 
 # Create your views here.
-
-
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -55,8 +55,6 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
-
-
 
 
 def product_detail(request, product_id):
