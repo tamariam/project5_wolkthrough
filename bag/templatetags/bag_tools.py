1e9e16f14774135190e447a9706fbdcc0eb080ef
sopiko abulidze <abulidze2019@gmail.com>
2024-03-26 14:23:13 +0000

    create checkout app

--- bag/templatetags/bag_tools.py
+++ bag/templatetags/bag_tools.py
@@ -1,14 +1,23 @@
+# from django import template
+
+# register = template.Library()
+
+# @register.filter(name='calc_subtotal')
+# def calc_subtotal(price, quantity):
+#     # Check if either price or quantity is a dictionary
+#     if isinstance(price, dict) or isinstance(quantity, dict):
+#         return 0  # Or any default value you prefer if one of them is a dictionary
+#     else:
+#         try:
+#             return price * quantity
+#         except TypeError:
+#             return 0
+
 from django import template
+
 
 register = template.Library()
 
 @register.filter(name='calc_subtotal')
 def calc_subtotal(price, quantity):
-    # Check if either price or quantity is a dictionary
-    if isinstance(price, dict) or isinstance(quantity, dict):
-        return 0  # Or any default value you prefer if one of them is a dictionary
-    else:
-        try:
-            return price * quantity
-        except TypeError:
-            return 0
+    return price * quantity
